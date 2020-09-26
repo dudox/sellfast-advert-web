e0aa1e71122a862f9a340b42bbc087406d331415
dudox <trybemark@gmail.com>
2020-09-26 19:38:22 +0100

    changed colors

--- resources/views/plans/premium.blade.php
+++ resources/views/plans/premium.blade.php
@@ -5,8 +5,8 @@
 </div>
 <div class="content" style="margin: 0px">
     <div class="pricing-table pricing-4">
-        <h3 class="pricing-value center-text bottom-10  bolder" style="background:#f83265">₦1500<sup>.00</sup></h3>
-        <h2 class="pricing-subtitle center-text bottom-30" style="background: #f83265">Bank transfer</h2>
+        <h3 class="pricing-value center-text bottom-10  bolder" style="background:#fbbc24">₦1500<sup>.00</sup></h3>
+        <h2 class="pricing-subtitle center-text bottom-30" style="background: #fbbc24">Bank transfer</h2>
         <ul class="pricing-list bottom-30">
             <li><i class="fa fa-check-circle "></i> You get two advert post for 30 days</li>
             <li><i class="fa fa-check-circle"></i> Your advert remain on our page for 30 days</li>
@@ -14,7 +14,7 @@
             <li><i class="fa fa-check-circle"></i> Customer price request notification</li>
             <li><i class="fa fa-check-circle"></i> High conversion rate</li>
         </ul>
-        <a href="https://payment.sellfast.ng/smart-plan-starter-pack" target="_blank" class="button button-s button-center-large button-rounded uppercase ultrabold" style="background:#f83265; color: #000;">Proceed to payment</a>
+        <a href="https://payment.sellfast.ng/smart-plan-starter-pack" target="_blank" class="button button-s button-center-large button-rounded uppercase ultrabold" style="background:#fbbc24; color: #000;">Proceed to payment</a>
     </div>
     <div class="clear"></div>
 </div>
